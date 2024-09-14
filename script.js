63ac83c10b45044e674e75c1f13ca24f07070227
Siglesias98 <155172953+Siglesias98@users.noreply.github.com>
2024-09-14 17:36:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function mostrarImagen() {
-    const imagenFloral = document.getElementById('imagenFloral');
-    imagenFloral.style.display = 'block'; // Muestra la imagen
+    const paginaInicio = document.getElementById('paginaInicio');
+    const imagenContainer = document.getElementById('imagenContainer');
+
+    // Añade la clase para deslizar la página hacia arriba
+    paginaInicio.classList.add('deslizar-arriba');
+
+    // Espera a que termine la animación antes de mostrar la imagen
+    setTimeout(() => {
+        paginaInicio.style.display = 'none'; // Oculta la página de inicio
+        imagenContainer.style.display = 'block'; // Muestra el contenedor de la imagen
+    }, 1000); // Tiempo de espera debe coincidir con la duración de la animación
 }
