af23c061b8560ce81dcccaf2ff9c5dea75fc8c42
Siglesias98 <155172953+Siglesias98@users.noreply.github.com>
2024-09-14 18:16:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,7 @@ function deslizarPagina() {
     const paginaInicio = document.getElementById('paginaInicio');
     const imagenContainer = document.getElementById('imagenContainer');
 
-    // Añade la clase para deslizar la página hacia arriba
-    paginaInicio.classList.add('deslizar-arriba');
-
-    // Espera a que termine la animación antes de mostrar la imagen
-    setTimeout(() => {
-        paginaInicio.style.display = 'none'; // Oculta la página de inicio
-        imagenContainer.style.display = 'flex'; // Muestra el contenedor de la imagen
-    }, 1000); // Tiempo de espera debe coincidir con la duración de la animación
+    // Ocultar la página de inicio y mostrar la imagen
+    paginaInicio.style.display = 'block';
+    imagenContainer.style.display = 'none';
 }
