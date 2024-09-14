733b59392255b1572cee1f5e2a560fbe9e3286d6
Siglesias98 <155172953+Siglesias98@users.noreply.github.com>
2024-09-14 18:07:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function deslizarPagina() {
     const paginaInicio = document.getElementById('paginaInicio');
+    const imagenContainer = document.getElementById('imagenContainer');
 
     // Añade la clase para deslizar la página hacia arriba
     paginaInicio.classList.add('deslizar-arriba');
 
-    // Opcional: Si deseas ocultar completamente el contenido después de la animación,
-    // puedes añadir un temporizador para eliminar el elemento después del deslizamiento
-    // setTimeout(() => {
-    //     paginaInicio.style.display = 'none'; // Oculta la página de inicio
-    // }, 1000); // Tiempo de espera debe coincidir con la duración de la animación
+    // Espera a que termine la animación antes de mostrar la imagen
+    setTimeout(() => {
+        paginaInicio.style.display = 'none'; // Oculta la página de inicio
+        imagenContainer.style.display = 'flex'; // Muestra el contenedor de la imagen
+    }, 1000); // Tiempo de espera debe coincidir con la duración de la animación
 }
